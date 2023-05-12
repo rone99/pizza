f475e7ae3eb10aafce175734e8a0b3abfefdc172
rone99 <128990756+rone99@users.noreply.github.com>
2023-05-12 16:44:33 +0530

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,16 @@
+function ron(){
+    var cheese=prompt("which type of pizza?");
+    if(cheese==="cheese"){
+        alert("price=55")
+    }
+    else{
+        alert("no such type avalable")
+
+    }
+
+    
+
+}
+function rex(){
+    
+}
